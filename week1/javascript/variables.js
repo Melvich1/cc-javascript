0a63f021541adbef4b7caf45990c55e775d1b645
Melvich1 <michaelajeware080@gmail.com>
2024-06-07 13:23:38 +0100

    first commit

---
+++ week1/javascript/variables.js
@@ -0,0 +1,64 @@
+//alert('External Javascript')
+
+//var title ='Professor Jega';
+//let firstName='Michael';
+//let commanderInChiefofTheArmedForces='Bola Ahmed Tinubu'
+const firstName = 'Michael'
+//alert(firstName);
+console.log(firstName);
+firstName='Joseph';
+//alert(Joseph);
+console.log(firstName);
+
+//Board
+const shapeOfBoard = 'rectangle';
+const colorOfBoard = 'white';
+const numberOfLegs = 3;
+let isMovable = 'true';
+let dusterIsMagnetic = 'true';
+
+//Fan
+let numberOfFans = 2;
+const typeOfFan = 'standing';
+const isStandingFan = 'true';
+
+//Table
+let tableIsMovable = 'true';
+const color = 'white';
+const legsOfTable = 4;
+
+//Nigeria
+let economicMovable = true;
+let militaryCorrupted = true;
+let presidentNonIntelligent = true;
+let houseOfRepNonIntelligent = true;
+let policeAbuse = true;
+let citizenStayfocusedOnPolitcs = false;
+const politiciansLooting = true;
+let educationBeAtLow = true;
+let currencyValueless = true;
+let governorsCorrupted = true;
+
+//Business Restaurant
+const goodBusinessProfitable = true;
+const badBusinessProfitable = false;
+const havingBoss = true;
+const havingClient = true;
+let muchCustomers = true;
+const tablesAndChair = true;
+let beautifulDesign = true;
+let comfort = true;
+const salesPerson = true;
+let securityPersonal = true;
+
+//Car
+let movingOnRoad = true;
+let havingFourTyres = true;
+const havingEnine = true;
+const needingMaintenance = true;
+const havingSparetyre = true;
+const roundTyre = true;
+const havingSeatBelt = true;
+let havingBackBoots = true;
+const havingEngineBelt = true;
+const bodyColorPainting = true;
